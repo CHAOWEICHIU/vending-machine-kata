440198843c69e3e1b64d2797ac2319af00ff6ebf
chao wei chiu <chaoweichiu@gmail.com>
2016-11-30 09:23:06 -0500

    added test for .insertCoins()

--- test/machine-spec.js
+++ test/machine-spec.js
@@ -1,12 +1,31 @@
 const expect = require('chai').expect
     , Machine = require('../lib/machine')
 
-describe('Vending Machine', ()=>{
+describe('machine', ()=>{
 
-  it('Machine', ()=>{
+  let machine, coins, msg
 
-    let machine = new Machine
-    expect(machine.displayMessage()).to.equal('INSERT COIN')
-    
+  beforeEach(()=>{
+    machine = new Machine
+    coins = ''
+    msg = ''
   })
+
+  describe('.displayMessage()', ()=>{
+    it('INSERT COIN without inserting coin', ()=>{
+
+      expect(machine.displayMessage()).to.equal('INSERT COIN')
+    })
+  })
+
+  describe('.insertCoins(coins).displayMessage()', ()=>{
+		it('display amount that is accepted, and rejected', ()=>{
+			coins = { quarters: 1, dimes : 1, nickels : 1, pennies : 2}
+			msg = 'Accepted Amount: 0.4, Rejected Amount: 0.02'
+			expect(machine.insertCoins(coins).displayMessage()).to.equal(msg)
+		})
+	})
+
+
+
 })
