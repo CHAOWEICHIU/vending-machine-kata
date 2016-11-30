5d731c30cf3b4e0da23d9cd99c02f5afb4a9a7f0
chao wei chiu <chaoweichiu@gmail.com>
2016-11-30 09:45:58 -0500

    pass

--- test/machine-spec.js
+++ test/machine-spec.js
@@ -3,40 +3,45 @@ const expect = require('chai').expect
 
 describe('machine', ()=>{
 
-  let machine, coins, msg
+  let machine, coins, msg, operation
 
   beforeEach(()=>{
     machine = new Machine
     coins = ''
     msg = ''
+    operation = ''
   })
 
   describe('.displayMessage()', ()=>{
     it('INSERT COIN without inserting coin', ()=>{
-      expect(machine.displayMessage()).to.equal('INSERT COIN')
+      msg       = 'INSERT COIN'
+      operation = machine.displayMessage()
+      expect(operation).to.equal(msg)
     })
   })
 
   describe('.insertCoins(coins).displayMessage()', ()=>{
 		it('display amount that is accepted, and rejected', ()=>{
-			coins = { quarters: 1, dimes : 1, nickels : 1, pennies : 2}
-			msg = 'Accepted Amount: 0.4, Rejected Amount: 0.02'
-			expect(machine.insertCoins(coins).displayMessage()).to.equal(msg)
+      msg = 'Accepted Amount: 0.4, Rejected Amount: 0.02'
+      coins = { quarters: 1, dimes : 1, nickels : 1, pennies : 2}
+      operation = machine.insertCoins(coins).displayMessage()
+			expect(operation).to.equal(msg)
 		})
 	})
 
   describe('.selectProduct()', ()=>{
 		it('Display proper message after Select Product without selecting a product', ()=>{
-			msg = 'cola: $1 [out of stock], chips: $0.5 [in stock], candy: $0.65 [in stock]'
-			expect(machine.selectProduct()).to.equal(msg)
+      msg = 'cola: $1 [out of stock], chips: $0.5 [in stock], candy: $0.65 [in stock]'
+      operation = machine.selectProduct()
+			expect(operation).to.equal(msg)
 		})
 	})
 
   describe('.selectProduct("chips").displayMessage()', ()=>{
 		it('Display proper message after selected product[in stock] with inadequate amoutn of money', ()=>{
-			operations = machine.selectProduct('chips').displayMessage()
-			msg = 'chips is $0.5, please insert $0.5'
-			expect(operations).to.equal(msg)
+      msg = 'chips is $0.5, please insert $0.5'
+      operation = machine.selectProduct('chips').displayMessage()
+			expect(operation).to.equal(msg)
 		})
 	})
 
